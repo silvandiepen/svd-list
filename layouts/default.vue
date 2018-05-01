a3a1b298e85c44770fd6df8ad9b488b7ccf34f1a
Sil <sil@matise.nl>
2018-05-01 13:04:45 +0200

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+div
+	header
+		row(center)
+			column(small="1", medium="2:3")
+				h3.logo lists
+	main.bg-white
+		<nuxt></nuxt>
+</template>
+<style>
+</style>
+
+<script>
+import headings from '~/static/head.json';
+// import Footer from 'svd-footer'
+
+
+export default{
+	head: headings
+}
+</script>
